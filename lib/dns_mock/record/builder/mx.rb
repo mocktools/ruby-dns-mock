9c225b73cad686b0f690eec5d42dae6559303988
Vladislav Trotsenko <admin@bestweb.com.ua>
2021-02-03 22:28:16 +0200

    Feature/Ability to specify MX record priority
    * Updated DnsMock::Record::Builder::Mx, tests
    * Updated gem version, readme, changelog

--- lib/dns_mock/record/builder/mx.rb
+++ lib/dns_mock/record/builder/mx.rb
@@ -4,17 +4,28 @@ module DnsMock
   module Record
     module Builder
       class Mx < DnsMock::Record::Builder::Base
+        include DnsMock::Error::Helper
+
+        MX_RECORD_REGEX_PATTERN = /\A(.+):(\d+)|(.+)\z/.freeze
         RECORD_PREFERENCE_STEP = 10
 
         def build
           records_data.map.with_index(1) do |record_data, record_preference|
+            record_data, custom_record_preference = parse_mx_record_data(record_data)
             target_factory.new(
               record_data: [
-                record_preference * DnsMock::Record::Builder::Mx::RECORD_PREFERENCE_STEP,
+                custom_record_preference&.to_i || record_preference * DnsMock::Record::Builder::Mx::RECORD_PREFERENCE_STEP,
                 record_data
               ]
             ).create
           end
+        end
+
+        private
+
+        def parse_mx_record_data(record_data)
+          raise_record_context_type_error(:mx, record_data, ::String)
+          record_data.scan(DnsMock::Record::Builder::Mx::MX_RECORD_REGEX_PATTERN).flatten.compact
         end
       end
     end
--- spec/dns_mock/record/builder/mx_spec.rb
+++ spec/dns_mock/record/builder/mx_spec.rb
@@ -5,25 +5,69 @@ RSpec.describe DnsMock::Record::Builder::Mx do
     subject(:builder_class) { described_class }
 
     it { is_expected.to be < DnsMock::Record::Builder::Base }
+    it { is_expected.to be_const_defined(:MX_RECORD_REGEX_PATTERN) }
     it { is_expected.to be_const_defined(:RECORD_PREFERENCE_STEP) }
+  end
+
+  describe 'MX_RECORD_REGEX_PATTERN' do
+    subject(:regex_pattern) { described_class::MX_RECORD_REGEX_PATTERN }
+
+    context 'when host with priority' do
+      subject(:string) { "#{host}:#{priority}" }
+
+      let(:host) { random_hostname }
+      let(:priority) { rand(100).to_s }
+
+      it { expect(regex_pattern.match?(string)).to be(true) }
+      it { expect(string[regex_pattern, 1]).to eq(host) }
+      it { expect(string[regex_pattern, 2]).to eq(priority) }
+    end
+
+    context 'when host without priority' do
+      subject(:string) { random_hostname }
+
+      it { expect(regex_pattern.match?(string)).to be(true) }
+      it { expect(string[regex_pattern, 3]).to eq(string) }
+    end
   end
 
   describe '.call' do
     subject(:builder) { described_class.call(target_factory, records_data) }
 
     let(:target_factory) { class_double('TargetFactory') }
-    let(:target_class_instance) { instance_double('TargetClass') }
-    let(:target_factory_instance) { instance_double('TargetFactory', create: target_class_instance) }
-    let(:records_data) { (0..2) }
 
-    it 'returns array of target class instances' do
-      [10, 20, 30].zip(records_data).each do |record_preference, record_data|
-        expect(target_factory)
-          .to receive(:new)
-          .with(record_data: [record_preference, record_data])
-          .and_return(target_factory_instance)
+    describe 'Success' do
+      context 'when valid records_data nested type' do
+        let(:target_class_instance) { instance_double('TargetClass') }
+        let(:target_factory_instance) { instance_double('TargetFactory', create: target_class_instance) }
+        let(:mx_host) { random_hostname }
+        let(:mx_priority) { 0 }
+        let(:records_data) { ["#{mx_host}:#{mx_priority}", 'b', 'c'] }
+
+        it 'returns array of target class instances' do
+          [10, 20, 30].zip(records_data).each_with_index do |(record_preference, record_data), index|
+            expect(target_factory)
+              .to receive(:new)
+              .with(record_data: index.zero? ? [mx_priority, mx_host] : [record_preference, record_data])
+              .and_return(target_factory_instance)
+          end
+          expect(builder).to eq(::Array.new(records_data.size) { target_class_instance })
+        end
       end
-      expect(builder).to eq(::Array.new(records_data.size) { target_class_instance })
+    end
+
+    describe 'Failure' do
+      context 'when invalid records_data nested type' do
+        let(:record_data_object) { 42 }
+        let(:records_data) { [record_data_object] }
+
+        it do
+          expect { builder }.to raise_error(
+            DnsMock::Error::RecordContextType,
+            "#{record_data_object.class} is invalid record context type for MX record. Should be a String"
+          )
+        end
+      end
     end
   end
 end
